293bdc9e69037fd84452df504b9afb38cbd900d2
Seogun95 <ksj083895@gmail.com>
2023-02-20 07:46:57 +0900

    design: footer 레이아웃 생성

---
+++ src/common/Footer.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const FooterWrapper = styled.footer`
+  ${(props) => props.theme.FlexRow};
+  background-color: ${(props) => props.theme.CL.dark_2};
+  width: calc(100% - 18rem);
+  margin-left: 18rem;
+  height: 100px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Copyright = styled.p`
+  font-size: 14px;
+  color: #999;
+`;
+
+export default function Footer() {
+  return (
+    <FooterWrapper>
+      <Copyright>&copy; 2023 서근개발노트. All rights reserved.</Copyright>
+    </FooterWrapper>
+  );
+}
--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import Sidebar from '../common/Sidebar';
+import Footer from '../common/Footer';
+
+const MainWrapper = styled.main`
+  ${(props) => props.theme.FlexRow};
+  width: calc(100% - 18rem);
+  height: 100%;
+  margin: 2rem 0 2rem 18rem;
+  padding-bottom: 100px;
+`;
 
 function Layout({ children }) {
   return (
     <div>
       <Sidebar />
+      <MainWrapper>{children}</MainWrapper>
+      <Footer />
     </div>
   );
 }
