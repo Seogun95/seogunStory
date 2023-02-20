91f7e139ece35cda63766530e352ac2b8c332f31
Seogun95 <ksj083895@gmail.com>
2023-02-20 13:25:19 +0900

    fix: 리듀서 네이밍 변경

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import postListSlice from '../modules/postListSlice';
+import postList from '../modules/postListSlice';
 
 const store = configureStore({
   reducer: {
-    postListSlice,
+    postList,
   },
 });
 
