ffe40ecda299ede3850fb14ecce4333f419f7530
Seogun95 <ksj083895@gmail.com>
2023-02-20 10:14:55 +0900

    feat: 모달버튼 구현

--- src/common/Sidebar.jsx
+++ src/common/Sidebar.jsx
@@ -1,17 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { FaUserAlt, FaPlus } from 'react-icons/fa';
+import Button from './Button';
+import Modal from '../components/Modal';
 
 const SidebarWrapper = styled.aside`
   background-color: ${(props) => props.theme.CL.dark_3};
   box-shadow: 4px 0px 5px 0px rgb(0 0 0 / 76%);
   width: 18rem;
-  height: 100%;
+  height: calc(100vh - 3.125rem);
   position: fixed;
-  top: 0;
+  top: 50px;
   left: 0;
   z-index: 1;
 `;
 
+const SidebarContent = styled.div`
+  ${(props) => props.theme.FlexCol}
+  justify-content: flex-start;
+  height: 100%;
+  width: 100%;
+  margin: 1rem 0;
+  gap: 2rem;
+`;
+
+const ImgContainer = styled.div`
+  position: relative;
+  ${(props) => props.theme.FlexRow}
+  width: 13.75rem;
+  height: 14.375rem;
+  border-radius: ${(props) => props.theme.BR.half_round};
+  overflow: hidden;
+  backdrop-filter: contrast(0.5);
+  img {
+    width: 100%;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background: #000000d1;
+    width: 100%;
+    height: 20%;
+    transition: 0.2s ease;
+  }
+  &:hover {
+    &:before {
+      background: #000000ea;
+    }
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  * {
+    color: white;
+  }
+`;
+
+const SidebarContainer = styled.div`
+  position: relative;
+  ${(props) => props.theme.FlexRow};
+  justify-content: center;
+  margin-top: ${(props) => props.margin};
+  p {
+    color: white;
+  }
+`;
+
+const Overlay = styled.div`
+  ${(props) => props.theme.FlexRow};
+  position: absolute;
+  top: -20px;
+  left: 0;
+  padding: 3px 1rem;
+  border-radius: 0.25rem;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+  color: white;
+  font-size: 0.8rem;
+`;
+
 export default function Sidebar() {
-  return <SidebarWrapper></SidebarWrapper>;
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal((prevShowModal) => !prevShowModal);
+  };
+
+  return (
+    <SidebarWrapper>
+      <SidebarContent>
+        <ImgContainer>
+          <img src="https://i.imgur.com/buI0G8V.png" alt="프로필" />
+          <IconWrapper>
+            <FaUserAlt />
+          </IconWrapper>
+        </ImgContainer>
+        <SidebarContainer>
+          <p>HELLO, 서근!</p>
+        </SidebarContainer>
+        <SidebarContainer margin={'2rem'}>
+          <Button large onClick={toggleModal}>
+            <FaPlus />
+            <Overlay>포스팅 하기</Overlay>
+          </Button>
+          <Modal state={showModal} setState={setShowModal}></Modal>
+        </SidebarContainer>
+      </SidebarContent>
+    </SidebarWrapper>
+  );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import useOutsideClick from '../hooks/useOutsideClick';
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: #000000ca;
+  backdrop-filter: blur(1px);
+  visibility: ${(props) => (props.show ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.show ? '1' : '0')};
+  transition: visibility 0.2s ease-in-out, opacity 0.2s ease-in-out;
+`;
+
+const ModalContent = styled.div`
+  background-color: #1f1f1f;
+  margin: 15% auto;
+  padding: 20px;
+  width: 40%;
+  min-height: 300px;
+  border-radius: 1rem;
+  filter: drop-shadow(2px 14px 30px rgba(0, 0, 0, 80%));
+  & {
+    color: white;
+  }
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 1.5rem;
+  color: white;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s ease all;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+function Modal(props) {
+  const modalRef = useRef(null);
+
+  const toggleModal = () => {
+    props.setState((prevShowModal) => !prevShowModal);
+  };
+
+  useOutsideClick(modalRef, () => {
+    props.setState(false);
+  });
+
+  useEffect(() => {
+    document.body.style.overflow = props.state ? 'hidden' : 'auto';
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [props.state]);
+
+  return (
+    <>
+      <ModalWrapper show={props.state}>
+        <ModalContent ref={modalRef}>
+          <CloseButton onClick={toggleModal}>
+            <FontAwesomeIcon icon={faTimes} />
+          </CloseButton>
+          {props.children}
+        </ModalContent>
+      </ModalWrapper>
+    </>
+  );
+}
+
+export default Modal;
