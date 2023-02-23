e2539f6914c8b97a1b3593dba8d21fa70654d373
Seogun95 <ksj083895@gmail.com>
2023-02-23 11:05:36 +0900

    fix: useInputOnChange hook 수정

--- src/hooks/useInputOnChange.jsx
+++ src/hooks/useInputOnChange.jsx
@@ -1,5 +1,7 @@
 import { useState, useCallback } from 'react';
 
+// 기존의 js에서 ts로 변경되어서 type오류 발생
+// 우선 any로 처리
 const useInputOnChange = (initialValue) => {
   const [state, setState] = useState(initialValue);
 
@@ -8,7 +10,10 @@ const useInputOnChange = (initialValue) => {
     setState((prev) => ({ ...prev, [name]: value }));
   }, []);
 
-  return [state, setStateHandler];
+  const reset = () => {
+    setState(initialValue);
+  };
+  return [state, setStateHandler, reset];
 };
 
 export default useInputOnChange;
